a30f2cb9db5a1fd49ea3b15b8bd5598f1e1227a5
Sandra <lilsan23@aol.com>
2018-06-07 18:48:28 -0500

    Final

--- src/main/java/org/launchcode/TechjobsApplication.java
+++ src/main/java/org/launchcode/TechjobsApplication.java
@@ -2,11 +2,12 @@ package org.launchcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TechjobsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechjobsApplication.class, args);
+		final ConfigurableApplicationContext run = SpringApplication.run(TechjobsApplication.class, args);
 	}
 }
